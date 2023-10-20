18c3060caca1bed8c8b7a475f155883955cdf251
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-10-20 14:00:28 +0300

    Create Apollo client and patch it (hack)

---
+++ src/lib/utilities/apolloClient.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch';
+import { ApolloClient } from '@apollo/client/core';
+import { HttpLink } from '@apollo/client/link/http';
+import { InMemoryCache } from '@apollo/client/cache';
+import { setContext } from '@apollo/client/link/context';
+import { ACCESS_TOKEN, GRAPHQL_ENDPOINT } from '$env/static/private';
+
+class Client {
+    constructor() {
+        if (Client._instance) {
+        }
+        Client._instance = this;
+
+        this.client = this.setupClient();
+    }
+
+    setupClient() {
+        const link = new HttpLink({
+            uri: process.env['GRAPHQL_ENDPOINT'],
+            fetch
+        });
+        const authLink = setContext((_, { headers }) => {
+            return {
+                headers: {
+                    ...headers,
+                    'X-Hasura-Admin-Secret': ACCESS_TOKEN
+                }
+            };
+        });
+        const client = new ApolloClient({
+            credentials: 'include',
+            link: authLink.concat(link),
+            cache: new InMemoryCache()
+        });
+        return client;
+    }
+}
